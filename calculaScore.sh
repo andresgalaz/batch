ee6263c0971a1d8b61b172cb2a45cbaff05cecb6
Andres Galaz <andres.galaz@gmail.com>
2016-11-29 09:50:30 -0300

    Variable ambiente por parametro

--- calculaScore.sh
+++ calculaScore.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
+if [ "$1" ] ; then
+	WSAPI_AMBIENTE="$1"
+	export WSAPI_AMBIENTE
+fi
+if [ ! "$WSAPI_AMBIENTE" ] ; then
+	echo "No existe variable de ambiente WSAPI_AMBIENTE"
+	exit 1
+fi
+echo '-----------------------------'
+echo "| WSAPI_AMBIENTE = $WSAPI_AMBIENTE"
+echo '-----------------------------'
 echo
-echo
-echo '--------------'
-echo 'Migra procesos'
-echo '--------------'
+echo '-----------------------------'
+echo '|  Migra procesos'
+echo '-----------------------------'
 cd /home/ubuntu/migraObservations
 node proceso.js
-echo '-------------------'
-echo 'Calcula Puntaje Mes'
-echo '-------------------'
+echo '-----------------------------'
+echo '|  Calcula Puntaje Mes'
+echo '-----------------------------'
 FECHA=$(date +'%Y-%m-%d')
 mysql  --user=snapcar --password=snapcar --database=score --table << EOF
 call calculaScoreMes('$FECHA');
